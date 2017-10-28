30f192011031d735f0f0617f8c8a6c4ecfd62427
Vladimir Maxymenko <462960@gmail.com>
2017-10-28 16:31:21 +0300

    Saga added

--- dev/store.js
+++ dev/store.js
@@ -1,11 +1,15 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import { rootReducer } from './reducers/rootReducer';
+import createSagaMiddleware from 'redux-saga';
 
 const defaultState = {
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const enhancers = compose(
-window.devToolsExtension && window.devToolsExtension()
+window.devToolsExtension && window.devToolsExtension(),
+applyMiddleware(sagaMiddleware)
 	);
 
 export const store = createStore(rootReducer, defaultState, enhancers);
