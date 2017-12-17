50b2edb706af9d5251782531796f9ab15cf5d7aa
Vladimir Maxymenko <462960@gmail.com>
2017-12-17 15:19:15 +0200

    No dubble chips

--- dev/helpers/api.js
+++ dev/helpers/api.js
@@ -19,13 +19,8 @@ axios.get(`${_baseURL}lat=${lat}&lon=${lon}${_APIKEY}`)
   .catch(err => console.log('Error:', err));
 
 
-  //http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=7&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
 
-  //http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
-
+  // Today
   //http://api.openweathermap.org/data/2.5/weather?q=London&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
   // 5 days
   //http://api.openweathermap.org/data/2.5/forecast?q=London&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
-  // Up to 16 days
-  //http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=7&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
-  // http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=5&appid=76c65d2a6d6a1d398c2ecea2ba15d67e
--- dev/sagas/sagas.js
+++ dev/sagas/sagas.js
@@ -1,27 +1,32 @@
 
 import { getForecastByName, getForecastByCoords } from '../helpers/api';
-import { put, call, all } from 'redux-saga/effects'
+import { put, call, all, select } from 'redux-saga/effects'
 import { takeLatest } from 'redux-saga';
 import { v4 } from 'node-uuid';
 import { LOAD_DATA_SUCCESS, LOAD_DATA_REQUEST, ADD_CHIP, LOAD_SET_ERROR } from '../helpers/constants';
 
-
 function* loadDataDetails({payload}) {
+  const getChips = state => state.chipsReducer;
   try {
     const data = yield call(getForecastByName, payload);
-    console.log(data.id);
+    // Select is the way to access store from saga
+    const chipSet = yield select(getChips);
+    // Check city existence in the chips array
+    const exists = chipSet.some(item => item.city == data.name);
     // Yields effect to the reducer specifying the action type and data details
     yield put({
       type: LOAD_DATA_SUCCESS,
       data
     });
-    yield put({
-      type: ADD_CHIP,
-      id: v4(),
-      city: data.name,
-      lat: data.coord.lat,
-      lon: data.coord.lon
-    })
+    // If chip with such a city exists do nothing
+    exists ? null
+      : yield put({
+        type: ADD_CHIP,
+        id: v4(),
+        city: data.name,
+        lat: data.coord.lat,
+        lon: data.coord.lon
+      })
   } catch (error) {
     throw error;
   }
