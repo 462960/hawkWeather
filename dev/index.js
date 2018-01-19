14fdf4528d731b606f84539c6d2ac8320b9ad371
Vladimir Maxymenko <462960@gmail.com>
2018-01-19 11:44:30 +0200

    Dev performance tool

--- dev/index.js
+++ dev/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./configureStore";
 import MainContainer from "Containers/MainContainer";
+var registerObserver = require('react-perf-devtool');
+registerObserver()
 
 const store = configureStore();
 
